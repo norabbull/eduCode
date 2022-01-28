3972116c41d11f3a9c524478a527032a8741dc1c
norabbull <norabbull@gmail.com>
2022-01-28 17:43:42 +0100

    update

--- Pandemi/sim_person.py
+++ Pandemi/sim_person.py
@@ -10,7 +10,7 @@ import numpy as np
 
 
 class Person:
-    def __init__(self,i, fart, tid_syk):
+    def __init__(self,i, fart=100, tid_syk=10, immunitet=0):
         """
         
         Tid regnes i "frames" som betyr en "runde" i simulasjonen.
@@ -22,12 +22,7 @@ class Person:
         #ID and name
         self.indeks = i
         self.navn = "Person " + str(i)
-        
-        #Status: Mottakelig, infisert eller friskmeldt
-        self.mottakelig = True   # S
-        self.infisert = False    # I
-        self.friskmeldt = False  # R
-        self.tid_infisert = -1        # "tidspunkt" for infeksjon
+    
         
         # Nåværende posisjon
         self.posX = np.random.random()*100
@@ -46,9 +41,10 @@ class Person:
         self.delta_posX = (self.nyposX - self.posX) / self.fart
         self.delta_posY = (self.nyposY - self.posY) / self.fart
 
-
-        # infeksjonstid, tid det tar å bli frisk
-        self.tid_syk = tid_syk
+        self.tid_syk = tid_syk            # Tid det tar å bli frisk
+        self.tid_immunitet = immunitet    # Hvor lenge man er immun
+        
+        self.sett_normaltilstand()
 
     def infiser(self,i):
         # Endre status til syk
@@ -62,13 +58,28 @@ class Person:
         self.friskmeldt = True
         self.mottakelig = False
         self.infisert = False
+        self.immunitet = self.tid_immunitet
+        
+    def sett_normaltilstand(self):
+        # Endre til start condition etter sykdom
+        self.infisert = False
+        self.mottakelig = True
+        self.friskmeldt = False
+        self.tid_infisert = -1
+        self.immunitet = self.tid_immunitet
         
     def oppdater_status(self,i):
         # Endre status til frisk dersom infeksjonstid er over
-        if self.tid_infisert > -1:
+        
+        if self.tid_infisert > -1:     # Er infisert
             if (i - self.tid_infisert) > self.tid_syk:
                 self.helbred()
-                               
+        elif self.friskmeldt:          # Har vært infisert for ikke lenge siden
+            if self.immunitet == 0:
+                self.sett_normaltilstand()
+            else:
+                self.immunitet -= 1
+            
     def sett_målposisjon(self):
         # Sett ny målposisjon
         self.nyposX = np.random.random()*100
--- Pandemi/sir_model_animation.py
+++ Pandemi/sir_model_animation.py
@@ -23,6 +23,7 @@ import random
 from sim_person import Person
 
 from matplotlib.animation import FuncAnimation
+import time
 # Enable interactive plot (jupyter)
 #%matplotlib notebook
 
@@ -31,29 +32,36 @@ from matplotlib.animation import FuncAnimation
     
 #%%
 
-# Startverdier
 
-tid = 200                # antll uker vi simulerer
-N = 500                  # antall personer i populasjonen
-p_start_smittede = 1     # prosentandel infiserte individer ved start (0-100%)
+
+##################################################################
+##  Startverdier - ENDRE PARAMETRE HER
+##################################################################
+
+
+tid = 200                   # antall uker vi simulerer
+N = 1000                     # antall personer i populasjonen
+p_start_smittede = 2        # prosentandel infiserte individer ved start (0-100%)
 start_smittede = int(N * p_start_smittede / 100)
-
-kontaktrate = 2          # smittsomhets radius. (i pixler, 0-100).
-p_smittsomhet = 100      # smittsomhet. sannsynlighet for å overføre sykdom (0-100%)
-
-p_karantene = 30         # prosentandel av populasjon i karantene (0-100%)
+kontaktrate = 3             # smittsomhets radius
+p_smittsomhet = 70         # smittsomhet. sannsynlighet for å overføre sykdom (0-100%)
+p_karantene = 0             # prosentandel av populasjon i karantene (0-100%)
 start_karantene = int(N * p_karantene / 100)
+tid_syk = 10                # Tid det tar å bli frisk igjen (0-uendelig)
+sykehuskapasitet = 300      # Antall personer som kan legges inn på sykehus
+fart = "tilfeldig"                   # 1 - 300. Alternativ: "tilfeldig"
+immunitet = 0              # Antall immunitetsdager
 
-tid_syk = 10             # Tid det tar å bli frisk igjen (0-uendelig)
+
+##################################################################
+##  IKKE ENDRE KODEN UNDER
+##################################################################
 
 S = [N-start_smittede]
 I = [start_smittede]
 R = [0]
 t = [0]
-
-fart = 200             # 1 - 300. Alternativ: "tilfeldig"
-
-
+R_tallet = [0]
 
 # Opprett populasjon:
 # sett alle populasjon på tilfeldige posisjoner. Infiser noen. 
@@ -62,12 +70,12 @@ populasjon = []
 for i in range(N):
     
     if fart == "tilfeldig": 
-        (np.random.random()+0.5) * 100
+        fart = (np.random.random()+0.5) * 100
     else: 
         fart = fart
     
     # Legge til person i populasjonen
-    p = Person(i, fart, tid_syk)
+    p = Person(i, fart = fart, tid_syk = tid_syk, immunitet=10)
     populasjon.append(p)
     
 # Smitt tilfeldige personer (antall like mange som smittede) og infiser
@@ -79,12 +87,19 @@ for p in random.sample(populasjon, start_karantene):
     p.sett_karantene()
 
 
-# grafikk / animasjon
-fig = plt.figure(figsize=(20,10))
-plot1 = fig.add_subplot(1,2,1)
-plot2 = fig.add_subplot(1,2,2)
+##################################################################
+##  Grafikk / animasjon 
+##################################################################
+
+# Opprett grafikk
+fig = plt.figure(figsize=(20,40))
+plot1 = fig.add_axes([0.1,0.1,0.4,0.8])
+plot2 = fig.add_axes([0.55,0.46,0.4,0.4])
+plot3 = fig.add_axes([0.55,0.15,0.4,0.2])
+
 plot1.axis('off')
 plot2.axis([0,tid,0,N+10])
+plot3.axis([0,tid,0,2])
 
 scatt = plot1.scatter([p.posX for p in populasjon],
                  [p.posY for p in populasjon],
@@ -93,13 +108,22 @@ scatt = plot1.scatter([p.posX for p in populasjon],
 
 fig2 = plt.Rectangle((0,0),100,100,fill=False)
 plot1.add_patch(fig2)
+
 plt_infiserte, = plot2.plot(start_smittede,color="red",label="Infiserte")
 plt_friskmeldte, = plot2.plot(start_smittede,color="lightblue",label="Friskmeldte")
 plt_mottakelige, = plot2.plot(N,color="green",label="Mottakelige")
-plot2.legend(handles = [plt_friskmeldte,plt_infiserte, plt_mottakelige])
+plt_R_tallet, = plot3.plot(R_tallet[0], color = "indigo", label = "R-tallet")
+plot2.axhline(sykehuskapasitet, color = "orange", label = "sykehuskapasitet")
+plot2.legend(handles = [plt_friskmeldte,plt_infiserte, plt_mottakelige, plt_R_tallet])
 plot2.set_xlabel("Tid")
 plot2.set_ylabel("Mennesker")
+plot3.set_xlabel("Tid")
+plot3.set_ylabel("R-tallet")
 
+
+##################################################################
+##  Simuler og animer verdier
+##################################################################
 
 # animer - oppdater verdier for hele populasjon og oppdater plottene
 def animer(frame,S, I, R,t, populasjon):
@@ -109,36 +133,42 @@ def animer(frame,S, I, R,t, populasjon):
     størrelser = [20 for p in populasjon]   # grafikk
     
     for p in populasjon:
+        
         # sjekk hvor lenge personen har vært syk
         p.oppdater_status(frame)
                        
         # animer forflytning for hver person
         p.oppdater_posisjon()
+                
+        # sjekk om det er personer i nærheten av en syk person og infiser alle
+        # innen infeksjonsradiusen (gitt sannsynlighet for smitte)
+        if p.infisert:
+            for p2 in populasjon:
+                if p2.indeks == p.indeks or p2.infisert or p2.friskmeldt: # Endre her om de skal kunne bli syke igjen
+                    pass
+                else:
+                    d = p.hent_dist(p2.posX, p2.posY)
+                    if d < kontaktrate:
+                        if np.random.random() < p_smittsomhet / 100:
+                            p2.infiser(frame)
+                            størrelser[p2.indeks] = 80
+
+
         if p.friskmeldt:
             friskmeldt += 1 # tell antall friskmeldte
         if p.infisert:
             smittede += 1 # tell antall infiserte
         
-            
-            # sjekk om det er personer i nærheten av en syk person og infiser alle
-            # innen infeksjonsradiusen (gitt sannsynlighet for smitte)
-            for person in populasjon:
-                if person.indeks == p.indeks or person.infisert or person.friskmeldt: # Endre her om de skal kunne bli syke igjen
-                    pass
-                else:
-                    d = p.hent_dist(person.posX,person.posY)
-                    if d < kontaktrate:
-                        if np.random.random() < p_smittsomhet / 100:
-                            person.infiser(frame)
-                            størrelser[person.indeks] = 80
-
-        farger.append(p.hent_farge()) #change dot color according to the person's status
-
+        farger.append(p.hent_farge())  # endre farge til person
+        
+    R_tall = (smittede - I[-1]) / I[-1]  # Gamle smittede / nye smittede  
+    
     #oppdater plottene
     I.append(smittede)
     R.append(friskmeldt)
     S.append(S[0]-I[-1])
     t.append(frame)
+    R_tallet.append(R_tall)
 
 
     # Overfør data to the matplotlib grafikk
@@ -150,14 +180,21 @@ def animer(frame,S, I, R,t, populasjon):
     plt_infiserte.set_data(t,I)
     plt_friskmeldte.set_data(t,R)
     plt_mottakelige.set_data(t,S)
-    return scatt,plt_infiserte,plt_friskmeldte, plt_mottakelige
+    plt_R_tallet.set_data(t,R_tallet)
+    #time.sleep(0.1)
+    return scatt,plt_infiserte,plt_friskmeldte, plt_mottakelige, plt_R_tallet
 
-# run the animation indefinitely
+
+
+##################################################################
+##  Kjør simulasjonen
+##################################################################
+
 animation = FuncAnimation(fig, 
                           animer,
                           frames = tid,
-                          interval=200,
-                          fargs=(S,I,R,t,populasjon),
-                          blit=False,
+                          interval = 200,
+                          fargs = (S,I,R,t,populasjon),
+                          blit = False,
                           repeat = False)
 plt.show()
